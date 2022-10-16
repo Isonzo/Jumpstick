cebdd62d4c414e20827b3e0c8c9f693e878f42a4
Isonzo <renato.soverina@gmail.com>
2022-10-16 18:26:13 -0300

    removed complexity from turret tracking for now

--- actors/enemies/Turret.gd
+++ actors/enemies/Turret.gd
@@ -1,7 +1,6 @@
 extends StaticBody2D
 
 const LERP_SPEED : float= 0.05
-const MAX_SPEED: float = 0.02
 
 var previous_angle: float = 0
 
@@ -22,17 +21,6 @@ func _physics_process(delta):
 func take_aim():
 	if player != null:
 		var current_angle = lerp_angle(barrel.rotation, (player.global_position - global_position).normalized().angle(), LERP_SPEED)
-		
-		$Label.text = "difference %f" % (current_angle - previous_angle)
-		
-		if abs(current_angle) - abs(previous_angle) < MAX_SPEED:
-			pass
-		elif current_angle > previous_angle:
-			current_angle = previous_angle + MAX_SPEED
-		elif current_angle < previous_angle:
-			current_angle = previous_angle - MAX_SPEED
-		
-		
 		
 		barrel.rotation = current_angle
 		
